3decf5926a2d242d07872599e9d7d5ca828352de
melanie <melaniejuliett@gmail.com>
2021-11-25 16:40:07 -0500

    Primera configuracion de Spring con mongo

---
+++ src/main/java/com/TheElegance/TheElegance/TheEleganceApplication.java
@@ -0,0 +1,13 @@
+package com.TheElegance.TheElegance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheEleganceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheEleganceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TheElegance/TheElegance/TheEleganceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheElegance.TheElegance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheEleganceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
